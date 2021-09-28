5625474db3685b8b1851fd4a90f1af7d6b747f2f
Priti Bhunia <pritibhunia@gmail.com>
2021-09-28 21:14:50 +0530

    Deploy on git

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-   <BrowserRouter basename="/priti-portfolio"><App /></BrowserRouter>
+   <BrowserRouter><App /></BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
